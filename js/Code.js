c1c16f378383ae4a23f2439df8f54fd3089798ab
lakshyashahi0712 <76860211+lakshyashahi0712@users.noreply.github.com>
2022-06-02 23:26:10 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "BUGS";
+const accessCode2 = "ARITHMETIC OPERATORS";
+const accessCode3 = "LOOPS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("U B S G", 100,50)
+    fill("lightblue")
+    text("Hint: The more you code, the more of me there is", 100,70)
+    text("I may be gone for now but you can't get rid of me forever !!", 130,85)
+
+    fill("white")
+    textSize(15)
+    text("M C T E I A I T R H   T R S O R E A P O ", 620,150)
+    fill("lightblue")
+    text("Hint: I perform addition,subtraction,multiplication,", 620,170)
+    text("Division,exponenetiation and modulus operation !!",655,185)
+
+    fill("white")
+    textSize(15)
+    text("O L P O S ", 100,250)
+    fill("lightblue")
+    text("Hint: I execute a set of repeated statements !!", 100,270)
+
+}
